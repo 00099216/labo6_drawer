047dade911fbd36f967dc12e98559bd29ae3d7ad
00099216 <32801207+00099216@users.noreply.github.com>
2018-05-14 22:52:40 -0600

    se agregrga clase modelo

---
+++ app/src/main/java/com/andres00099216/labo6_drawer/modelo.java
@@ -0,0 +1,41 @@
+package com.andres00099216.labo6_drawer;
+
+/**
+ * Created by Andres on 14/5/2018.
+ */
+
+public class modelo {
+    private String nombre;
+    private String descripcion;
+    private int foto;
+
+    public modelo(String nombre, String descripcion, int foto) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.foto = foto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getFoto() {
+        return foto;
+    }
+
+    public void setFoto(int foto) {
+        this.foto = foto;
+    }
+}
